67529ccb6dd9c7e75c14626f2251da80fe86e30a
yessine <yessine.ag@gmail.com>
2023-07-05 13:20:01 +0200

    message

--- server.js
+++ server.js
@@ -7,10 +7,25 @@ const server = dgram.createSocket('udp4');
 
 // Handle incoming UDP packets
 server.on('message', (message, remote) => {
-  const gpsData = message.toString('utf8');
-  // Process and store the GPS data
-  console.log(gpsData);
+  const data = message.toString('utf8');
+  console.log(`Received data from ${remote.address}:${remote.port}: ${data}`);
+
+  // Process the received data based on your application requirements
+  // Example: Split the data by comma to extract specific information
+  const dataParts = data.split(',');
+
+  // Example: Extract latitude and longitude from the data
+  const latitude = dataParts[3];
+  const longitude = dataParts[4];
+
+  // Example: Perform further actions with the extracted data
+  console.log(`Latitude: ${latitude}`);
+  console.log(`Longitude: ${longitude}`);
+
+  // Example: Store the data in a database or trigger other operations
+  // ...
 });
+
 
 // Start the server and listen for incoming UDP packets
 server.bind(PORT, () => {
